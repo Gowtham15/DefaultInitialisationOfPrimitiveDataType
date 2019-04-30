c03aa51e871adcf185de036d0d0f2ea3ce508109
gowtham15 <ch.gowtham@gmail.com>
2019-04-30 22:19:01 +0100

    Default Initialisation of Primitive Datatypes repo

---
+++ src/love/programming/DefaulInitialisationOfPrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+package love.programming;
+
+public class DefaulInitialisationOfPrimitiveDataTypes {
+    static boolean myBoolean;
+    static int myInt;
+    static short myShort;
+    static long myLong;
+    static float myFloat;
+    static double myDouble;
+    static char myChar;
+
+    public static void main(String[] args) {
+
+        int localInt;
+        //System.out.println("local int = "+localInt);
+        System.out.println("my boolean = "+myBoolean);
+        System.out.println("my Int = "+myInt);
+        System.out.println("my short = "+myShort);
+        System.out.println("my long = "+myLong);
+        System.out.println("my float = "+myFloat);
+        System.out.println("my double = "+myDouble);
+        System.out.println("my char = "+myChar);
+
+    }
+}
